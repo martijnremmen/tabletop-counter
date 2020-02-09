cbc3b4a1a6a6a673b6b416482108f194435062a3
Accendit <36697826+Accendit@users.noreply.github.com>
2020-02-09 23:53:33 +0100

    Latest change is more prominent and left to right

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="history">
-    <small v-for="(change, index) in getLastFive" :key="index">{{
-      change
-    }}</small>
+    <small
+      v-for="(change, index) in getLastFive"
+      :key="index"
+      :class="index == 0 ? 'first' : ''"
+      >{{ change }}</small
+    >
   </div>
 </template>
 
@@ -16,7 +19,7 @@ export default Vue.extend({
 
   computed: {
     getLastFive(): Number[] {
-      return this.history.slice(Math.max(this.history.length - 5, 0));
+      return this.history.slice(Math.max(this.history.length - 5, 0)).reverse();
     }
   }
 });
@@ -25,7 +28,14 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .history {
   max-width: 100%;
-  height: 100%;
+  height: 2em;
+  text-align: left;
+  padding-left: 1em;
+  padding-right: 1em;
+
+  .first {
+    font-size: 1.3rem;
+  }
 
   small {
     padding: 0.2em;
