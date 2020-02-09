fa1b39a2ac8b14f16e51db3a25be95f74b055ab8
Accendit <36697826+Accendit@users.noreply.github.com>
2020-02-09 23:51:10 +0100

    Added SFC's for Modals

---
+++ src/components/Modal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="modalmask">
+    <div class="modal">
+      <div class="modalheader">
+        <div class="costumheader">
+          <slot name="header">Modal header</slot>
+        </div>
+
+        <button class="material-icons" @click="$emit('close')">close</button>
+      </div>
+      <div class="modalbody">
+        <slot name="body"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.modalmask {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba($color: #000000, $alpha: 0.5);
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+
+.modal {
+  width: 90%;
+  max-width: 40rem;
+  margin: 10% auto auto auto;
+  background-color: var(--bg-primary);
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 0px 0px 50px black;
+  z-index: 120;
+
+  .modalbody {
+    padding: 2em;
+  }
+
+  .modalheader {
+    background-color: var(--bg-secondary);
+    padding-left: 2em;
+
+    & * {
+      display: inline-block;
+    }
+
+    .costumheader {
+      width: (100% / 5 * 4);
+    }
+
+    button {
+      float: right;
+      background-color: transparent;
+      color: var(--text-red);
+      font-size: 2.6rem;
+      padding: 0.7rem;
+    }
+  }
+}
+</style>
---
+++ src/components/PlayerNameModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <Modal @close="$emit('close')">
+    <template v-slot:header>
+      <h2>Change player name</h2>
+    </template>
+    <template v-slot:body>
+      <p>What name would you like to give to {{ player.name }}?</p>
+      <label for="playername">New name</label>
+      <input type="text" placeholder="Name" id="playername" :value="newName" />
+      <button class="btn-primary" @click="$emit('submitChanges', newName)">
+        Submit
+      </button>
+    </template>
+  </Modal>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Player } from "../classes/Player";
+import Modal from "./Modal.vue";
+
+export default Vue.extend({
+  props: {
+    player: { type: Object as () => Player },
+    newName: String
+  },
+
+  components: {
+    Modal
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+input {
+  padding: 1em;
+  border: var(--bg-secondary);
+  background-color: var(--bg-darker);
+  border-radius: 15px;
+  color: var(--text);
+}
+
+::placeholder {
+  font-size: 1.2em;
+  color: var(--text);
+}
+</style>
