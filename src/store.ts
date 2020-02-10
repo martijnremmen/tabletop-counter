d030a7d4e344287c4bc60818643f54e6f9554629
Accendit <36697826+Accendit@users.noreply.github.com>
2020-02-10 21:48:30 +0100

    Commented out debug logs

--- src/store.ts
+++ src/store.ts
@@ -15,17 +15,17 @@ import {
 } from "./classes/Counter";
 
 export const store = Vue.observable({
-  debug: true,
+  debug: false,
   players: [] as Player[]
 });
 
 export const mutations = {
   addPlayer(name: string, health: number = 20): void {
-    if (store.debug) {
-      console.log(
-        `Adding player ${name} with starting health ${health.toString()}`
-      );
-    }
+    // if (store.debug) {
+    //   console.log(
+    //     `Adding player ${name} with starting health ${health.toString()}`
+    //   );
+    // }
     store.players.push(createPlayer(name, health));
   },
 
@@ -37,43 +37,43 @@ export const mutations = {
   },
 
   removePlayerById(id: number): void {
-    if (store.debug) {
-      console.log(`Removing player with ID ${id.toString()}`);
-    }
+    // if (store.debug) {
+    //   console.log(`Removing player with ID ${id.toString()}`);
+    // }
     store.players.splice(id, 1);
   },
 
   addCounterToPlayer(player: Player, type: string, value: number): void {
-    if (store.debug) {
-      console.log(
-        `Adding a new counter with type ${type} and starting value of ${value.toString()} to player ${
-          player.name
-        }`
-      );
-    }
+    // if (store.debug) {
+    //   console.log(
+    //     `Adding a new counter with type ${type} and starting value of ${value.toString()} to player ${
+    //       player.name
+    //     }`
+    //   );
+    // }
     addCounter(player, type, value);
   },
 
   removeCounterFromPlayer(player: Player, index: number): void {
-    if (store.debug) {
-      console.log(
-        `Removing counter id ${index.toString()} from player ${player.name}`
-      );
-    }
+    // if (store.debug) {
+    //   console.log(
+    //     `Removing counter id ${index.toString()} from player ${player.name}`
+    //   );
+    // }
     removeCounter(player, index);
   },
 
   incrementCounterFromPlayer(counter: Counter, value: number): void {
-    if (store.debug) {
-      console.log(`Incrementing counter ${counter.type} with ${value}`);
-    }
+    // if (store.debug) {
+    //   console.log(`Incrementing counter ${counter.type} with ${value}`);
+    // }
     incrementCounter(counter, value);
   },
 
   decrementCounterFromPlayer(counter: Counter, value: number): void {
-    if (store.debug) {
-      console.log(`Decrementing counter ${counter.type} with ${value}`);
-    }
+    // if (store.debug) {
+    //   console.log(`Decrementing counter ${counter.type} with ${value}`);
+    // }
     decrementCounter(counter, value);
   },
 
@@ -82,9 +82,9 @@ export const mutations = {
   },
 
   updatePlayerName(player: Player, name: string): void {
-    if (store.debug) {
-      console.log(`Changing ${player.name}'s name to ${name.toString()}`);
-    }
+    // if (store.debug) {
+    //   console.log(`Changing ${player.name}'s name to ${name.toString()}`);
+    // }
     changeName(player, name);
   }
 };
